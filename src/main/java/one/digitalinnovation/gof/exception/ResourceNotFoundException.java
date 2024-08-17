e5371d9848b8bedc11e5afe1cf8b5c39fb9ae718
Mc-Nanes <lon@poli.br>
2024-08-17 03:02:23 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/gof/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.gof.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -0,0 +1,43 @@
+package one.digitalinnovation.gof.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@NotNull(message = "Nome não pode ser nulo")
+    @Size(min = 1, message = "Nome não pode estar vazio")
+	private String nome;
+	@ManyToOne
+	private Endereco endereco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+}
